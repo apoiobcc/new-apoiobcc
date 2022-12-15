bec230b160d445befc42a5fcd23a5670a642107d
José Lucas Silva Mayer <josemayer@usp.br>
2022-12-15 17:51:16 -0400

    Adiciona a seção de criação dos módulos
    Signed-off-by: José Lucas Silva Mayer <josemayer@usp.br>

--- run.sh
+++ run.sh
@@ -6,6 +6,8 @@ fi
 
 if [ "$INSTALLED" != "true" ]; then
   sleep 40;
+  export COMPOSER_MEMORY_LIMIT=-1;
+  composer install
   php artisan key:generate
   php artisan config:cache
   php artisan install --ready
